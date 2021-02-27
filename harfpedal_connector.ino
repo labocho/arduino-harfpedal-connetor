f2af335cac8717b1fc6e2763ca400707a9f9e0c0
labocho <labocho@penguinlab.jp>
2021-02-27 10:28:22 +0900

    send midi event

--- harfpedal_connector.ino
+++ harfpedal_connector.ino
@@ -1,7 +1,7 @@
-void setup() {
-  Serial.begin(9600);
-  pinMode(8, INPUT);
-}
+// https://github.com/FortySevenEffects/arduino_midi_library
+#include <MIDI.h>
+
+MIDI_CREATE_DEFAULT_INSTANCE();
 
 // ROLAND:
 // 5V を TIP、D7 を RING、A0 を SLEEVE に
@@ -17,20 +17,24 @@ const int INTERVAL = 100;
 
 int last_value = -1;
 
-void loop() {
-  // int d = 0;
-  // d = digitalRead(8);
-  // Serial.println(d);
+void setup() {
+  Serial.begin(9600);
+  pinMode(8, INPUT);
+  MIDI.begin(1);
+  MIDI.turnThruOff();
+}
 
-  int a = 0;
-  a = analogRead(0);
-  //  Serial.println(a);
-  long v = a;
+void loop() {
+  long v = analogRead(0);
+  // Serial.println(v);
+
   if (v < VOL_MIN) v = VOL_MIN;
   if (VOL_MAX < v) v = VOL_MAX;
   v = (v - VOL_MIN) * 127 / (VOL_MAX - VOL_MIN);
+
   if (v != last_value) {
-    Serial.println(v);
+    // Serial.println(v);
+    MIDI.sendControlChange(64, v, 1);
     last_value = v;
   }
 
